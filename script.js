b1014ebbd0dfc27b612e28b577248a52de60d012
RomanasV <romanasvenckus@gmail.com>
2022-10-26 15:23:15 +0300

    script.js file refactored

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 fetch('https://jsonplaceholder.typicode.com/posts?_limit=15&_embed=comments&_expand=user')
   .then(res => res.json())
   .then(posts => {
-    console.log(posts);
     let postsList = document.querySelector('#posts-list');
 
     posts.map(post => {
@@ -14,6 +13,7 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15&_embed=comments&_exp
 
       let postAuthorElement = document.createElement('span');
       postAuthorElement.classList.add('post-author');
+      postAuthorElement.innerHTML = `Author: <a href="#">${post.user.name}</a>`;
 
       let postContentElement = document.createElement('p');
       postContentElement.classList.add('post-content');
@@ -28,43 +28,21 @@ fetch('https://jsonplaceholder.typicode.com/posts?_limit=15&_embed=comments&_exp
 
       let commentsListElement = document.createElement('div');
       commentsListElement.classList.add('comments-list');
+      
+      post.comments.map(comment => {
+        let commentItem = document.createElement('div');
+        commentItem.classList.add('comment-item');
+
+        commentItem.innerHTML = `<h4 class="comment-title">${comment.name}</h4>
+                                 <span class="comment-author">Commented by: ${comment.email}</span>
+                                 <p class="comment-content">${comment.body}</p>`;
+
+        commentsListElement.append(commentItem);
+      })
 
       commentsWrapperElement.append(commentsSectionTitle, commentsListElement);
 
       postItem.append(postTitleElement, postAuthorElement, postContentElement, commentsWrapperElement);
       postsList.append(postItem);
-
-
-      console.log(post.user.name);
-
-      fetch('https://jsonplaceholder.typicode.com/users/' + post.userId)
-        .then(res => res.json())
-        .then(user => {
-          postAuthorElement.innerHTML = `Author: <a href="#">${user.name}</a>`;
-        })
-
-      console.log(post);
-      console.log(post.comments);
-      post.comments.map(comment => {
-        console.log(comment);
-        console.log(comment.name);
-        console.log(comment.body);
-        console.log(comment.email);
-      })
-
-      fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
-        .then(res => res.json())
-        .then(comments => {
-          comments.map(comment => {
-            let commentItem = document.createElement('div');
-            commentItem.classList.add('comment-item');
-
-            commentItem.innerHTML = `<h4 class="comment-title">${comment.name}</h4>
-                                     <span class="comment-author">Commented by: ${comment.email}</span>
-                                     <p class="comment-content">${comment.body}</p>`;
-
-            commentsListElement.append(commentItem);
-          })
-        })
     })
   })
