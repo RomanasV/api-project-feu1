e33e6a9b5ac2ea2589a3fc45f2d5f6c957d01e0c
RomanasV <romanasvenckus@gmail.com>
2022-10-31 12:05:36 +0200

    render search results function created

--- search.js
+++ search.js
@@ -47,68 +47,90 @@ function init() {
   fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`)
     .then(res => res.json())
     .then(posts => {
-      if (posts.length > 0) {
-        const postsWrapper = document.createElement('div');
-        postsWrapper.classList.add('posts-wrapper', 'search-result-wrapper');
-        searchResults.append(postsWrapper);
+      // if (posts.length > 0) {
+      //   const postsWrapper = document.createElement('div');
+      //   postsWrapper.classList.add('posts-wrapper', 'search-result-wrapper');
+      //   searchResults.append(postsWrapper);
 
-        const postsWrapperTitle = document.createElement('h2');
-        postsWrapperTitle.classList.add('search-wrapper-title');
-        postsWrapperTitle.textContent = 'Posts:';
+      //   const postsWrapperTitle = document.createElement('h2');
+      //   postsWrapperTitle.classList.add('search-wrapper-title');
+      //   postsWrapperTitle.textContent = 'Posts:';
 
-        const postsList = document.createElement('ul');
-        postsList.classList.add('search-list');
+      //   const postsList = document.createElement('ul');
+      //   postsList.classList.add('search-list');
 
-        postsWrapper.append(postsWrapperTitle, postsList);
+      //   postsWrapper.append(postsWrapperTitle, postsList);
 
-        posts.map(post => {
-          const postItem = document.createElement('li');
-          postItem.classList.add('search-item');
+      //   posts.map(post => {
+      //     const postItem = document.createElement('li');
+      //     postItem.classList.add('search-item');
   
-          const postLink = document.createElement('a');
-          postLink.textContent = post.title;
-          postLink.href = './post.html?post_id=' + post.id;
+      //     const postLink = document.createElement('a');
+      //     postLink.textContent = post.title;
+      //     postLink.href = './post.html?post_id=' + post.id;
   
-          postItem.append(postLink);
-          postsList.append(postItem);
-        })
-      } else {
-        console.log('No users...')
+      //     postItem.append(postLink);
+      //     postsList.append(postItem);
+      //   })
+      // } else {
+      //   console.log('No users...')
+      // }
+
+      const params = {
+        data: posts,
+        parentElement: searchResults,
+        title: 'Posts:',
+        path: 'post',
       }
+
+      renderSearchResults(params);
     })
 
   fetch(`https://jsonplaceholder.typicode.com/albums?q=${search}`)
     .then(res => res.json())
     .then(albums => {
-      if (albums.length > 0) {
-        const albumsWrapper = document.createElement('div');
-        albumsWrapper.classList.add('albums-wrapper', 'search-result-wrapper');
-        searchResults.append(albumsWrapper);
+      const params = {
+        data: albums,
+        parentElement: searchResults,
+        title: 'Albums:',
+        path: 'album',
+      };
 
-        const albumsWrapperTitle = document.createElement('h2');
-        albumsWrapperTitle.classList.add('search-wrapper-title');
-        albumsWrapperTitle.textContent = 'Albums:';
-
-        const albumsList = document.createElement('ul');
-        albumsList.classList.add('search-list');
-
-        albumsWrapper.append(albumsWrapperTitle, albumsList);
-
-        albums.map(album => {
-          const albumItem = document.createElement('li');
-          albumItem.classList.add('album-item');
-  
-          const albumLink = document.createElement('a');
-          albumLink.textContent = album.title;
-          albumLink.href = './album.html?album_id=' + album.id;
-
-          albumItem.append(albumLink);
-          albumsList.append(albumItem);
-        })
-      } else {
-        console.log('No albums...')
-      }
+      renderSearchResults(params);
     })
+}
+
+function renderSearchResults(paramsObj) {
+  let { data, parentElement, title, path } = paramsObj;
+
+  if (data.length > 0) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('search-result-wrapper');
+    parentElement.append(wrapper);
+
+    const wrapperTitle = document.createElement('h2');
+    wrapperTitle.classList.add('search-wrapper-title');
+    wrapperTitle.textContent = title;
+
+    const list = document.createElement('ul');
+    list.classList.add('search-list');
+
+    wrapper.append(wrapperTitle, list);
+
+    data.map(item => {
+      const itemElement = document.createElement('li');
+      itemElement.classList.add('search-list-item');
+
+      const linkElement = document.createElement('a');
+      linkElement.textContent = item.title;
+      linkElement.href = `./${path}.html?${path}_id=${item.id}`;
+
+      itemElement.append(linkElement);
+      list.append(itemElement);
+    })
+  } else {
+    console.log('No albums...')
+  }
 }
 
 init();
