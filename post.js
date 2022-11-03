836d44edbc83d93109c91236fa4163176880c22f
RomanasV <romanasvenckus@gmail.com>
2022-11-03 10:32:15 +0200

    post page code refactored

--- post.js
+++ post.js
@@ -1,51 +1,63 @@
-const postId = 55;
+async function init() {
+  const queryParams = document.location.search;
+  const urlParams = new URLSearchParams(queryParams);
+  const postId = urlParams.get('post_id');
+  
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}?_expand=user&_embed=comments`);
+  const post = await res.json();
 
-fetch(`https://jsonplaceholder.typicode.com/posts/${postId}?_expand=user&_embed=comments`)
-  .then(res => res.json())
-  .then(post => {
-    let postWrapper = document.querySelector('#post-wrapper');
+  let postWrapper = document.querySelector('#post-wrapper');
 
-    let postTitleElement = document.createElement('h2');
-    postTitleElement.classList.add('post-title');
-    postTitleElement.textContent = post.title;
+  renderSinglePost(post, postWrapper);
+  renderAllComments(post, postWrapper);
+}
 
-    let postAuthorElement = document.createElement('span');
-    postAuthorElement.classList.add('post-author');
-    postAuthorElement.innerHTML = `Author: <a href="./user.html?user_id=${post.user.id}">${post.user.name}</a>`;
+function renderSinglePost(post, postWrapper) {
+  let postTitleElement = document.createElement('h2');
+  postTitleElement.classList.add('post-title');
+  postTitleElement.textContent = post.title;
 
-    let postContentElement = document.createElement('p');
-    postContentElement.classList.add('post-content');
-    postContentElement.textContent = post.body;
+  let postAuthorElement = document.createElement('span');
+  postAuthorElement.classList.add('post-author');
+  postAuthorElement.innerHTML = `Author: <a href="./user.html?user_id=${post.user.id}">${post.user.name}</a>`;
 
-    let commentsWrapperElement = document.createElement('div');
-    commentsWrapperElement.classList.add('comments-wrapper');
+  let postContentElement = document.createElement('p');
+  postContentElement.classList.add('post-content');
+  postContentElement.textContent = post.body;
 
-    let commentsSectionTitle = document.createElement('h3');
-    commentsSectionTitle.classList.add('comments-section-title');
-    commentsSectionTitle.textContent = 'Comments:';
+  postWrapper.append(postTitleElement, postAuthorElement, postContentElement);
+}
 
-    let commentsListElement = document.createElement('div');
-    commentsListElement.classList.add('comments-list');
-    
-    post.comments.map(comment => {
-      let commentItem = document.createElement('div');
-      commentItem.classList.add('comment-item');
+function renderAllComments(post, postWrapper) {
+  let commentsWrapperElement = document.createElement('div');
+  commentsWrapperElement.classList.add('comments-wrapper');
 
-      commentItem.innerHTML = `<h4 class="comment-title">${comment.name}</h4>
-                                <span class="comment-author">Commented by: ${comment.email}</span>
-                               <p class="comment-content">${comment.body}</p>`;
+  let commentsSectionTitle = document.createElement('h3');
+  commentsSectionTitle.classList.add('comments-section-title');
+  commentsSectionTitle.textContent = 'Comments:';
 
-      commentsListElement.append(commentItem);
-    })
+  let commentsListElement = document.createElement('div');
+  commentsListElement.classList.add('comments-list');
+  
+  post.comments.map(comment => {
+    let commentItem = document.createElement('div');
+    commentItem.classList.add('comment-item');
 
-    commentsWrapperElement.append(commentsSectionTitle, commentsListElement);
+    commentItem.innerHTML = `<h4 class="comment-title">${comment.name}</h4>
+                              <span class="comment-author">Comment's author: ${comment.email}</span>
+                              <p class="comment-content">${comment.body}</p>`;
 
-    postWrapper.append(postTitleElement, postAuthorElement, postContentElement, commentsWrapperElement);
-
-    let moreAuthorPostsElement = document.createElement('a');
-    moreAuthorPostsElement.classList.add('more-posts');
-    moreAuthorPostsElement.href = './posts.html?user_id=' + post.user.id;
-    moreAuthorPostsElement.textContent = `Other posts of ${post.user.name}`;
-
-    postWrapper.append(moreAuthorPostsElement);
+    commentsListElement.append(commentItem);
   })
+
+  commentsWrapperElement.append(commentsSectionTitle, commentsListElement);
+
+  let moreAuthorPostsElement = document.createElement('a');
+  moreAuthorPostsElement.classList.add('more-posts');
+  moreAuthorPostsElement.href = './posts.html?user_id=' + post.user.id;
+  moreAuthorPostsElement.textContent = `Other posts of ${post.user.name}`;
+
+  postWrapper.append(commentsWrapperElement, moreAuthorPostsElement);
+}
+
+init();
