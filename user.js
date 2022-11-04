912413d31492e407890c0aa789622d795881d38a
RomanasV <romanasvenckus@gmail.com>
2022-11-04 11:38:29 +0200

    user page refactored

--- user.js
+++ user.js
@@ -1,12 +1,23 @@
 import renderHeader from './header.js';
-import { createLinksList, fetchData, firstLetterUpperCase, getUrlParam } from './functions.js';
+import { createElement, createLinksList, fetchData, firstLetterUpperCase, getUrlParam } from './functions.js';
 
 async function init() {
   const userId = getUrlParam('user_id');
 
-  const userInfo = document.querySelector('.user-info');
+  const contentElement = document.querySelector('#content');
+  const user = await fetchData(`https://jsonplaceholder.typicode.com/users/${userId}?_embed=posts&_embed=albums`);
 
-  const user = await fetchData('https://jsonplaceholder.typicode.com/users/' + userId);
+  const userInfoElement = createUserInfoElement(user);
+  const userPostsElement = createUserPostsElement(user.posts);
+  const userAlbumsElement = createUserAlbumsElement(user.albums);
+
+  contentElement.append(userInfoElement, userPostsElement, userAlbumsElement);
+
+  renderHeader();
+}
+
+function createUserInfoElement(user) {
+  const userInfo = createElement('div', '', 'user-info');
 
   let { name, username, email, website, phone, company } = user;
   let { street, suite, city, zipcode } = user.address;
@@ -19,57 +30,41 @@ async function init() {
                           <li><strong>Website:</strong> <a href="https://${website}" target="_blank">${website}</a></li>
                           <li><strong>Work:</strong> ${company.name}</li>
                         </ul>`;
-  
-  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
-    .then(res => res.json())
-    .then(posts => {
-      const postsWrapper = document.querySelector('#posts-wrapper');
-  
-      const postsTitle = document.createElement('h3');
-      postsTitle.classList.add('posts-title');
-      postsTitle.textContent = 'User Posts:';
-  
-      const postsList = document.createElement('div');
-      postsList.classList.add('posts-list');
-  
-      postsWrapper.append(postsTitle, postsList);
-  
-      posts.map(post => {
-        const postItem = document.createElement('div');
-        postItem.classList.add('post-item');
-  
-        postItem.innerHTML = `<h4>${firstLetterUpperCase(post.title)}</h4>
-                              <p>${firstLetterUpperCase(post.body)}</p>
-                              <a href="./post.html?post_id=${post.id}">Read more</a>`;
-  
-        postsList.append(postItem);
-      })
-    })
-  
-  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums`)
-    .then(res => res.json())
-    .then(albums => {
-      const albumsWrapper = document.querySelector('#albums-wrapper');
-  
-      const albumsTitle = document.createElement('h3');
-      albumsTitle.classList.add('albums-title');
-      albumsTitle.textContent = 'User Albums:';
-  
-      const albumsListElement = createLinksList({
-        data: albums,
-        path: 'album',
-        listClasses: ['albums-list'],
-        itemClasses: ['album-item'],
-      });
-  
-      albumsWrapper.append(albumsTitle, albumsListElement);
-    })
-  
-  renderHeader();
+  return userInfo;
 }
 
-function createUserInfoElement() {
-  
+function createUserPostsElement(posts) {
+  const postsWrapper = createElement('div', '', 'posts-wrapper');
+  const postsTitle = createElement('h3', 'User Posts:', 'posts-title');
+  const postsList = createElement('div', '', 'posts-list');
+
+  postsWrapper.append(postsTitle, postsList);
+
+  posts.map(post => {
+    const postItem = createElement('div', '', 'post-item');
+
+    postItem.innerHTML = `<h4>${firstLetterUpperCase(post.title)}</h4>
+                          <p>${firstLetterUpperCase(post.body)}</p>
+                          <a href="./post.html?post_id=${post.id}">Read more</a>`;
+
+    postsList.append(postItem);
+  })
+
+  return postsWrapper;
+}
+
+function createUserAlbumsElement(albums) {
+  const albumsWrapper = createElement('div', '', 'albums-wrapper');
+  const albumsTitle = createElement('h3', 'User Albums:', 'albums-title');
+  const albumsListElement = createLinksList({
+    data: albums,
+    path: 'album',
+    listClasses: ['albums-list'],
+    itemClasses: ['album-item'],
+  });
+
+  albumsWrapper.append(albumsTitle, albumsListElement);
+  return albumsWrapper;
 }
 
 init();
