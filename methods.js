b0a214f1c46a8572a42560a1b3af2d0719055ceb
RomanasV <romanasvenckus@gmail.com>
2022-11-04 13:28:21 +0200

    12th task added

---
+++ methods.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'Pavadinimas',
+    body: 'posto body',
+    userId: 4,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    console.log(data.body)
+    console.log(data.title)
+    console.log(data.userId)
+    console.log(data.id)
+  });
